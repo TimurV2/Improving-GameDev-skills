862b09e8748896035559705df6538be38c567096
TimurV2 <sabzerov303@gmail.com>
2021-07-03 22:42:50 +0300

    Добавил фоновую музыку с возможность ставить на паузу и уменьшавить/увеличивать громкость

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from tkinter import *
 root = tk.Tk()
 
+
 screen_width = root.winfo_screenwidth()             # | Узнаём высоту и ширину экрана пользователя
 screen_height = root.winfo_screenheight()           # |
 screen_size = (screen_width, screen_height)
@@ -14,7 +15,6 @@ class GameOfLife:
     def __init__(self, complexity, x, y, speed):
         self.complexity = complexity               # Выбираем сложность и узнаём с установкой размер окна пользователя
         self.screen = pygame.display.set_mode(screen_size)
-
         self.x = x
         self.y = y
         self.speed = speed
@@ -25,13 +25,19 @@ class GameOfLife:
     def run(self):
 
         pygame.init()
-        flpause = False
         clock = pygame.time.Clock()
         pygame.display.set_caption('Выжить на сессии')
         running = True
-        pygame.mixer.init(44100, 16, 2, 4096)
-        pygame.mixer.music.load('DOOM.mp3')
-        pygame.mixer.music.play(-1)
+
+        file = 'secret.ogg'                             #
+        pygame.mixer.pre_init(44100, 32, 2, 4096)       #
+        pygame.mixer.init()                             #
+        pygame.mixer.music.load(file)                   # Настройка микшера для проигрывания, музыка ставится на паузу с помощью ПРОБЕЛА
+        pygame.mixer.music.play(-1)                     #
+        pygame.mixer.Sound(file)                        #
+        flpause = False                                 #
+        volume = 1                                      #
+
         while running:
 
             clock.tick(60)
@@ -40,17 +46,25 @@ class GameOfLife:
                 if event.type == pygame.QUIT:
                     running = False
 
-            if event.key == pygame.K_SPACE:
-                flpause = not flpause
-                if flpause:
-                    pygame.mixer.music.pause()
-                else:
-                    pygame.mixer.music.unpause()
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_SPACE:
+                        flpause = not flpause
+                        if flpause:
+                            pygame.mixer.music.pause()
+                        else:
+                            pygame.mixer.music.unpause()
+                    elif event.key == pygame.K_1:               # Убавление звука на 1
+                        volume = volume - 0.1
+                        pygame.mixer.music.set_volume(volume)
+                    elif event.key == pygame.K_2:               # Прибавление звука на 2
+                        volume = volume + 0.1
+                        pygame.mixer.music.set_volume(volume)
 
             pygame.display.update()
-            back_ground_img = pygame.image.load('kosmos-art-hellsescapeartist-tylercreatesworlds.jpg')
+            back_ground_img = pygame.image.load('kosmos-art-hellsescapeartist-tylercreatesworlds.jpg')  # Ставим задний фон
             self.screen.blit(back_ground_img, (0, 0))
-            self.movement()
+            self.movement()     # Вызываем функцию движения персонажа (пока что просто зелёный шар)
+
         pygame.quit()
 
     def movement(self):                 # Здесь прописано движение персонажа игрока с его прорисовкой
@@ -58,16 +72,12 @@ class GameOfLife:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
             self.x += self.speed
-
         if keys[pygame.K_LEFT]:
             self.x -= self.speed
-
         if keys[pygame.K_UP]:
             self.y -= self.speed
-
         if keys[pygame.K_DOWN]:
             self.y += self.speed
-
         pygame.draw.circle(self.screen, pygame.Color("Green"), (self.x, self.y), 50)
 
 
--- test.py
+++ test.py
@@ -1,7 +1,37 @@
 import pygame
-from pygame import *
-file = 'DOOM.mp3'
-pygame.mixer.pre_init(44100, 16, 2, 4096)
-pygame.mixer.init(44100, 16, 2, 4096)
-pygame.mixer.music.load(file)
-pygame.mixer.music.play(-1)
+import sys
+
+sc = pygame.display.set_mode((400, 300))
+pygame.init()
+pygame.mixer.music.load('secret.ogg')
+pygame.mixer.music.set_volume(1)
+pygame.mixer.music.play()
+pygame.mixer.pre_init(44100, 32, 2, 4096)
+pygame.mixer.init()
+
+flpause = True
+volume = 1
+
+while 1:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            sys.exit()
+
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_SPACE:
+                flpause = not flpause
+                if flpause:
+                    pygame.mixer.music.pause()
+                else:
+                    pygame.mixer.music.unpause()
+            elif i.key == pygame.K_1:
+                volume = volume - 0.1
+                pygame.mixer.music.set_volume(volume)
+            elif i.key == pygame.K_2:
+                volume = volume + 0.1
+                pygame.mixer.music.set_volume(volume)
+
+
+    pygame.time.delay(20)
+
+              
